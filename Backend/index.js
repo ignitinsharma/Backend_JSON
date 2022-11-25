784be5b38d03a623d48b4b91b2360cd12912c156
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-11-25 22:38:46 +0530

    added

---
+++ Backend/index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const app= express();
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.listen(process.env.PORT||4000,()=>{
+    console.log("app is running")
+})
